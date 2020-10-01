37dec4f0bfa2d0739d5931aa6cce348786b10800
Anthony Schafrik <anthonyschafrik@gmail.com>
2020-10-01 09:45:56 -0400

    Added in file uploader

--- server_ui/src/App.tsx
+++ server_ui/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Routes from './Routes';
 
 function App() {
-  return <div>server ui</div>;
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
